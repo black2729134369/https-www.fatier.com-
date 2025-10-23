394e00b2ab1b406082de123797ab7bb6adad09bb
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-23 16:27:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,651 @@
+<?php require 'data/configxhxh1.php';?>
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<title>法帝独立蒸烤集成灶_集成水槽洗碗机_十大品牌加盟代理-嵊州市法帝电器有限公司</title>
+		<script type="text/javascript" src="https://new.tjtjdetail.com/xhjishu.js"></script>
+		<meta name="keywords" content="集成灶,独立蒸烤,独立蒸烤集成灶,集成水槽,集成水槽洗碗机,集成灶10大品牌,集成灶十大品牌" />
+		<meta name="description" content="法帝厨房电器是一家浙江嵊州集成环保灶生产厂家，主营独立蒸烤集成灶，蒸烤一体集成灶、油烟机、燃气灶、消毒柜、热水器、烤箱、蒸箱、集成水槽、集成水槽洗碗机等多个厨卫电器产品，并荣获多个集成灶十大品牌殊荣，一线品牌法帝集成灶欢迎大家加盟代理！" />
+	</head>
+
+	<head>
+	<meta charset="utf-8" />
+	<meta name="applicable-device" content="pc">
+	<meta http-equiv="Content-Language" content="zh-CN" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="renderer" content="webkit">
+	<meta name="force-rendering" content="webkit">
+	<link rel="stylesheet" href="/template/yangshi/css/head.css">
+	<link rel="stylesheet" href="/template/yangshi/css/foot.css">
+	<link rel="stylesheet" href="/template/yangshi/css/aoyun.css">
+	<link rel="stylesheet" href="/template/yangshi/css/animate.css">
+	<link rel="stylesheet" href="/template/yangshi/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/template/yangshi/swiper-4.3.5/css/swiper.min.css">
+	<link rel="stylesheet" href="/template/yangshi/swiper-4.3.5/css/swiper.css">
+	<link rel="stylesheet" href="/template/yangshi/css/style.css">
+</head>
+<body>
+	<!-- 头部导航 -->
+	<div class="dhl">
+		<div class="header">
+			<a href="/"><img src="/static/upload/image/20200915/1600140339192540.png" class="headlogo" /></a>
+
+			<ul class="headnav">
+				<li class="headnav1"><a href="/">首页</a></li>
+				
+				<li class="headnav1">
+					<a class="red" href="/">产品中心</a>
+					
+					<div class="headnav2">
+						<ul>
+							
+							<li class="headnav22">
+								<a class="red" href="/youyanji/"><img src="/static/upload/image/20200915/1600154212881332.png" />油烟机</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/ranqizao/"><img src="/static/upload/image/20200915/1600154232304663.png" />燃气灶</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/xiaodugui/"><img src="/static/upload/image/20200915/1600154261502070.png" />消毒柜</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/jcprzx/"><img src="/static/upload/image/20231017/1697504497125784.png" />集成烹饪中心</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/reshuiqi/"><img src="/static/upload/image/20200915/1600154275430818.png" />热水器</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/diankaoxiang/"><img src="/static/upload/image/20200915/1600154313750516.png" />电烤箱</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/dianzhengxiang/"><img src="/static/upload/image/20200915/1600154302340448.png" />电蒸箱</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/jichengzao/"><img src="/static/upload/image/20231018/1697592061640500.png" />集成灶</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/shuicaoxiwanji/"><img src="/static/upload/image/20200915/1600154325107733.png" />集成水槽</a>
+							</li>
+							
+						</ul>
+					</div>
+					
+				</li>
+				
+				<li class="headnav1">
+					<a class="red" href="/about/">企业介绍</a>
+					
+					<div class="headnav2">
+						<ul>
+							
+							<li class="headnav22">
+								<a class="red" href="/about/"><img src="/static/upload/image/20200915/1600154479433655.png" />企业简介</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/pingpai/"><img src="/static/upload/image/20200915/1600154496973649.png" />品牌实力</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/wenhua/"><img src="/static/upload/image/20200915/1600154508678343.png" />法帝文化</a>
+							</li>
+							
+						</ul>
+					</div>
+					
+				</li>
+				
+				<li class="headnav1">
+					<a class="red" href="/article/">新闻资讯</a>
+					
+					<div class="headnav2">
+						<ul>
+							
+							<li class="headnav22">
+								<a class="red" href="/news/"><img src="/static/upload/image/20200915/1600155082767630.png" />法帝快讯</a>
+							</li>
+							
+							<li class="headnav22">
+								<a class="red" href="/video/"><img src="/static/upload/image/20200915/1600155098692590.png" />视频中心</a>
+							</li>
+							
+						</ul>
+					</div>
+					
+				</li>
+				
+				<li class="headnav1">
+					<a class="red" href="/jiameng/">招商加盟</a>
+					
+				</li>
+				
+				<li class="headnav1">
+					<a class="red" href="/contact/">联系我们</a>
+					
+				</li>
+				
+			</ul>
+			<div class="headertb">
+				<a href="https://mall.jd.com/index-10045948.html" target="_blank" rel="nofollow">
+					<img src="/template/yangshi/images/jd.png" alt="法帝京东官方旗舰店" title="法帝京东官方旗舰店"></a>
+			</div>
+		</div>
+	</div>
+<!--导航栏高度-->
+	<div class="dhlgd"></div>
+
+	<!-- 幻灯片 -->
+	<div class="swiper-container">
+		<div class="swiper-wrapper">
+			
+			<div class="swiper-slide">
+				<a href="">
+					<img src="/static/upload/image/20250609/1749431361470018.jpg" class="d-block w-100" alt="">
+				</a>
+			</div>
+			
+			<div class="swiper-slide">
+				<a href="">
+					<img src="/static/upload/image/20250609/1749431373218439.jpg" class="d-block w-100" alt="">
+				</a>
+			</div>
+			
+			<div class="swiper-slide">
+				<a href="">
+					<img src="/static/upload/image/20240802/1722561833744170.jpg" class="d-block w-100" alt="">
+				</a>
+			</div>
+			
+		</div>
+		<!-- <div class="swiper-button-prev d-none d-md-block"></div>
+	左箭头
+	<div class="swiper-button-next d-none d-md-block"></div>
+	右箭头-->
+		<div class="swiper-pagination"></div>
+	</div>
+
+	<!--产品推荐-->
+	<div class="index-t">
+		<ul class="index-tj">
+			
+			<li class="index-tj1">
+				<a href="/youyanji/"><img src="/static/upload/image/20200915/1600154212881332.png" />油烟机</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/ranqizao/"><img src="/static/upload/image/20200915/1600154232304663.png" />燃气灶</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/xiaodugui/"><img src="/static/upload/image/20200915/1600154261502070.png" />消毒柜</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/jcprzx/"><img src="/static/upload/image/20231017/1697504497125784.png" />集成烹饪中心</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/reshuiqi/"><img src="/static/upload/image/20200915/1600154275430818.png" />热水器</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/diankaoxiang/"><img src="/static/upload/image/20200915/1600154313750516.png" />电烤箱</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/dianzhengxiang/"><img src="/static/upload/image/20200915/1600154302340448.png" />电蒸箱</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/jichengzao/"><img src="/static/upload/image/20231018/1697592061640500.png" />集成灶</a>
+			</li>
+			
+			<li class="index-tj1">
+				<a href="/shuicaoxiwanji/"><img src="/static/upload/image/20200915/1600154325107733.png" />集成水槽</a>
+			</li>
+			
+		</ul>
+	</div>
+
+	<!-- 4款主推产品系列 -->
+	<section class="index1">
+		<ul>
+			<li class="index-cpimg1"> <a href="/youyanji/"> <img src="/template/yangshi/images/xiyouyanji.jpg" alt=""> </a>
+			</li>
+			<li class="index-cpimg1"> <a href="/ranqizao/"> <img src="/template/yangshi/images/ranqizaoju.jpg" alt=""> </a>
+			</li>
+			<li class="index-cpimg1"> <a href="/xiaodugui/"> <img src="/template/yangshi/images/qianrushixiaodugui.jpg" alt=""> </a>
+			</li>
+			<li class="index-cpimg1"> <a href="/reshuiqi/"> <img src="/template/yangshi/images/reshuiqi.jpg" alt=""> </a>
+			</li>
+		</ul>
+	</section>
+
+
+
+	<!-- 首页关于我们 -->
+	<section class="index2">
+		<img src="/template/yangshi/images/indexaboutbj.jpg" class="index2bj" />
+		<div class="index2main">
+			<ul class="index2mainlb">
+				<li class="index2mainlb1">
+					<div class="index2mainlb1left">
+						<i class="iconfont icon-fuwulinian"></i>
+					</div>
+					<div class="index2mainlb1right">
+						<h4>产品理念</h4>
+						<p>时尚 品质 科技 低碳</p>
+					</div>
+				</li>
+				<li class="index2mainlb1">
+					<div class="index2mainlb1left">
+						<i class="iconfont icon-fuwu"></i>
+					</div>
+					<div class="index2mainlb1right">
+						<h4>服务宗旨</h4>
+						<p>全程为客户提供最优质的服务，免去一切烦恼</p>
+					</div>
+				</li>
+				<li class="index2mainlb1">
+					<div class="index2mainlb1left">
+						<i class="iconfont icon-mubiaoyuanjing-01-01"></i>
+					</div>
+					<div class="index2mainlb1right">
+						<h4>企业愿景</h4>
+						<p>做一个让社会尊敬的有责任企业</p>
+					</div>
+				</li>
+				<li class="index2mainlb1">
+					<div class="index2mainlb1left">
+						<i class="iconfont icon-shouhou"></i>
+					</div>
+					<div class="index2mainlb1right">
+						<h4>完善的售后</h4>
+						<p>法帝客户服务中心会进行跟踪回访。</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- 首页重点导向图 -->
+	<section class="index3">
+		<div class="index3main">
+			<ul class="index3main1">
+				<li class="index3main2"> <a href="/news/138.html"> <img src="/template/yangshi/images/xpss.jpg">
+						<h5>新品上市</h5> <i>NEW ARRIVAL</i>
+					</a> </li>
+				<li class="index3main2"> <a href="/video/"> <img src="/template/yangshi/images/fdsp.jpg">
+						<h5>法帝视频</h5> <i>FATIER VIDEO</i>
+					</a> </li>
+				<li class="index3main2"> <a href="/news/"> <img src="/template/yangshi/images/fdsh.jpg">
+						<h5>法帝生活馆</h5> <i>FATIER LIFE</i>
+					</a> </li>
+			</ul>
+		</div>
+	</section>
+
+
+
+
+	<!-- 新闻动态 -->
+	<section class="index4">
+		<div class="index4news">
+			
+			<h4>新闻资讯</h4>
+			<h5>- NEWS INFORMATION -</h5>
+
+			
+			<div class="index4min">
+				
+				<div class="index4min1">
+					<div class="index4min2">
+						<a href="/news/1396.html" ><img src="/static/upload/image/20251023/1761180522165364.jpg" /><h4>告别等待，全时恒温：法帝16L36零冷水燃气热水器，定义沐浴自由</h4></a>
+						<a href="/news/1396.html" ><p>当温暖的水流从花洒中倾泻而下，包裹全身的瞬间，一整天的压力与疲惫也随之消散。法帝全新推出零冷水燃气热水器16L36，守护你随时··· 2025-10-23</p></a>
+					</div>
+				</div>
+				
+				<div class="index4min1">
+					<div class="index4min2">
+						<a href="/news/1394.html" ><img src="/static/upload/image/20250925/1758786712946791.jpg" /><h4>关于 2025 中秋国庆放假通知</h4></a>
+						<a href="/news/1394.html" ><p> 2025-09-25</p></a>
+					</div>
+				</div>
+				
+				<div class="index4min1">
+					<div class="index4min2">
+						<a href="/news/1391.html" ><img src="/static/upload/image/20250911/1757557140721697.jpg" /><h4>即开即热，澎湃畅浴！法帝16L36零冷水燃气热水器，幸福一触即发～</h4></a>
+						<a href="/news/1391.html" ><p>用水体验直接影响生活幸福感：早起洗漱时，刚打开水龙头的冷水让人一激灵；晚上给孩子洗澡，放半天冷水才能等到热水，水量小，水··· 2025-09-11</p></a>
+					</div>
+				</div>
+				
+			</div>
+			
+			<div class="index4gg">
+				<a href="/article/" class="text-gengduo">查看更多</a>
+			</div>
+			
+		</div>
+	</section>
+
+
+<!-- 您可能需要 -->
+
+	<section class="index5">
+		<div class="index5main">
+			
+			<ul class="index5main1">
+				<li class="index5main2"> <a href="/pingpai"> <img src="/template/yangshi/images/2_1039022671.png">
+						<h4>法帝品牌</h4>
+						<p>什么是法帝？法帝品牌如何？</p>
+						<h5>法帝品牌</h5>
+					</a> </li>
+				<li class="index5main2"> <a href="/news"> <img src="/template/yangshi/images/2_1039026942.png">
+						<h4>法帝头条</h4>
+						<p>了解和获取法帝最新活动资讯</p>
+						<h5>法帝头条</h5>
+					</a> </li>
+				<li class="index5main2"> <a href="/jiameng"> <img src="/template/yangshi/images/2_1039021863.png">
+						<h4>我要加盟</h4>
+						<p>加盟咨询热线：400-188-9090</p>
+						<h5>我要加盟</h5>
+					</a> </li>
+				<li class="index5main2"> <a href="/contact"> <img src="/template/yangshi/images/2_1039022674.png">
+						<h4>服务中心</h4>
+						<p>免费为您提供厨电问题的解答</p>
+						<h5>服务中心</h5>
+					</a> </li>
+			</ul>
+			<div class="clearfix"></div>
+		</div>
+	</section>
+
+
+	<script src="/template/yangshi/swiper-4.3.5/js/swiper.min.js"></script>
+	<script>
+		var mySwiper = new Swiper('.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			speed: 0,
+			autoplay: {
+				delay: 6500,
+				disableOnInteraction: false
+			},
+
+
+			// 如果需要分页器
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+
+			// 如果需要前进后退按钮
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+
+		})
+	</script>
+
+	<!-- 底部 -->
+<div class="footcolor">
+	<!-- 三个底部栏目 -->
+	<div class="footer">
+		<ul>
+			<li class="footer3">
+				<a href="/news/381.html">
+					<h3>清蒸净烤 · 智厨慧影</h3>
+					<h4>全新2.0智慧屏 烹饪娱乐双享受</h4>
+					<p>立即前往></p>
+				</a>
+			</li>
+			<li class="footer3">
+				<a href="/jiameng">
+					<h3>法帝优势</h3>
+					<h4>为提供健康和谐的家居环境打造健康厨电品牌，持续创新!</h4>
+					<p>立即前往></p>
+				</a>
+			</li>
+			<li class="footer3">
+				<a href="/news/510.html">
+					<h3>法帝24H+焕新计划</h3>
+					<h4>再创佳绩！终端爆单连连！！！</h4>
+					<p>立即前往></p>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<!-- 底部栏目导航 -->
+	<div class="footerdh">
+		<ul>
+			<li class="footerdh2">
+				<h5>关于法帝</h5>
+				<a href="/about">企业简介</a>
+				<a href="/pingpai">品牌故事</a>
+				<a href="/wenhua">企业文化</a>
+			</li>
+			<li class="footerdh2">
+				<h5>产品中心</h5>
+				<a href="/jcprzx">集成烹饪中心</a>
+				<a href="/jichengzao">法帝集成灶</a>
+				<a href="/youyanji">法帝油烟机</a>
+				<a href="/ranqizao">法帝燃气灶</a>
+				<a href="/xiaodugui">法帝消毒柜</a>
+				<a href="/reshuiqi">法帝热水器</a>
+				<a href="/dianzhengxiang">法帝电蒸箱</a>
+				<a href="/diankaoxiang">法帝电烤箱</a>
+				<a href="/shuicaoxiwanji">集成水槽</a>
+			</li>
+			<li class="footerdh2">
+				<h5>招商加盟</h5>
+				<a href="/jiameng">加盟优势</a>
+				<a href="/jiameng">加盟条件</a>
+				<a href="/jiameng">加盟支持</a>
+				<a href="/jiameng">加盟流程</a>
+				<a href="/jiameng">在线加盟</a>
+			</li>
+			<li class="footerdh2">
+				<h5>新闻中心</h5>
+				<a href="/news">精彩动态</a>
+				<a href="/video">视频中心</a>
+			</li>
+			<div class="wx">
+				<img src="/static/upload/image/20200909/1599617005847559.jpg" />
+				<h4>法帝官方微信平台</h4>
+				<p>欢迎您的关注！</p>
+			</div>
+		</ul>
+	</div>
+	<!-- 底部备案 -->
+	<div class="footerbeian">
+		<p> &nbsp
+			<a target="_blank" rel="nofollow" href="https://beian.miit.gov.cn/">备案号：浙ICP备12044723号-1</a>
+			&nbsp
+			<a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33068302000341"
+			 style="display:inline-block;"><img src="/template/yangshi/images/ba.png" />浙公网安备 33068302000341号</a>
+		</p>
+	</div>
+</div>
+<!-- 在线客服 -->
+
+
+<div class="suspension">
+	<div class="suspension-box">
+		<a href="#" class="a a-service "><i class="i"></i></a>
+		<a href="javascript:;" class="a a-service-phone "><i class="i"></i></a>
+		<a href="javascript:;" class="a a-qrcode"><i class="i"></i></a>
+	<!-- 	<a href="#" class="a a-cart"><i class="i"></i></a> -->
+		<a href="javascript:;" class="a a-top"><i class="i"></i></a>
+		<div class="d d-service">
+			<i class="arrow"></i>
+			<div class="inner-box">
+				<div class="d-service-item clearfix">
+					<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=3128254814&site=qq&menu=yes" class="clearfix"><span class="circle"><i class="i-qq"></i></span><h3>咨询在线客服</h3></a>
+				</div>
+			</div>
+		</div>
+		<div class="d d-service-phone">
+			<i class="arrow"></i>
+			<div class="inner-box">
+				<div class="d-service-item clearfix">
+					<span class="circle"><i class="i-tel"></i></span>
+					<div class="text">
+						<p>服务热线</p>
+						<p class="qq-red number">18858539083</p>
+					</div>
+				</div>
+				<div class="d-service-intro clearfix">
+					<p><i></i>加盟和代理</p>
+					<p><i></i>价格和优惠</p>
+					<p><i></i>获取产品资料</p>
+				</div>
+			</div>
+		</div>
+		<div class="d d-qrcode">
+			<i class="arrow"></i>
+			<div class="inner-box">
+				<div class="qrcode-img"><img src="/template/yangshi/images/side_ewm.jpg" alt="微信服务号"></div>
+				<p>微信服务号</p>
+			</div>
+		</div>
+
+
+
+
+	</div>
+</div>
+
+<script src="/template/yangshi/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+	/* ----- 侧边悬浮 ---- */
+	$(document).on("mouseenter", ".suspension .a", function(){
+		var _this = $(this);
+		var s = $(".suspension");
+		var isService = _this.hasClass("a-service");
+		var isServicePhone = _this.hasClass("a-service-phone");
+		var isQrcode = _this.hasClass("a-qrcode");
+		if(isService){ s.find(".d-service").show().siblings(".d").hide();}
+		if(isServicePhone){ s.find(".d-service-phone").show().siblings(".d").hide();}
+		if(isQrcode){ s.find(".d-qrcode").show().siblings(".d").hide();}
+	});
+	$(document).on("mouseleave", ".suspension, .suspension .a-top", function(){
+		$(".suspension").find(".d").hide();
+	});
+	$(document).on("mouseenter", ".suspension .a-top", function(){
+		$(".suspension").find(".d").hide(); 
+	});
+	$(document).on("click", ".suspension .a-top", function(){
+		$("html,body").animate({scrollTop: 0});
+	});
+	$(window).scroll(function(){
+		var st = $(document).scrollTop();
+		var $top = $(".suspension .a-top");
+		if(st > 400){
+			$top.css({display: 'block'});
+		}else{
+			if ($top.is(":visible")) {
+				$top.hide();
+			}
+		}
+	});
+	
+});	
+</script>
+
+
+
+</body>
+</html>
+
+<div class="yqlj">
+<div class="yqlj1">
+	<p>友情链接：</p>
+
+
+<a href="http://www.bzyfzm.com"  target="_blank" >滨州路灯厂家</a>
+
+
+
+<a href="http://www.dnwxc.com"  target="_blank" >电脑维修</a>
+
+
+
+<a href="http://www.shjiuyue.com"  target="_blank" >电动叉车轮胎</a>
+
+
+
+<a href="http://www.charlie.com.cn"  target="_blank" >环体包装机</a>
+
+
+
+<a href="http://www.ukrubens.com"  target="_blank" >暖气片</a>
+
+
+
+<a href="http://www.373zd.com"  target="_blank" >振动筛</a>
+
+
+
+<a href="http://www.yunzhenxuan.com"  target="_blank" >中式设计</a>
+
+
+
+<a href="http://www.office8008.com/"  target="_blank" >创意园出租</a>
+
+
+
+<a href="http://www.thwine.com"  target="_blank" >红酒加盟</a>
+
+
+
+<a href="http://www.zjyibei.com"  target="_blank" >汽车椅套</a>
+
+
+
+<a href="http://www.kefeiyajiaju.com"  target="_blank" >郑州办公家具厂</a>
+
+
+
+<a href="https://www.jiazhumeiguo.com"  target="_blank" >家住美国</a>
+
+
+
+<a href="http://hshzghz.com"  target="_blank" >黄山会议公司</a>
+
+
+
+<a href="http://www.seesjhj.com"  target="_blank" >合肥装修公司</a>
+
+
+
+<a href="http://www.shpyds.com"  target="_blank" >雕塑</a>
+
+
+
+<a href="http://www.meizhuozg.com"  target="_blank" >洗砂机</a>
+
+
+
+<a href="http://www.jm-xy.com"  target="_blank" >金银花烘干机</a>
+
+
+
+<a href="http://www.geruiweilan.com"  target="_blank" >锌钢护栏</a>
+
+
+
+<a href="http://www.ermacn.com"  target="_blank" >直播代运营</a>
+
+
+</div></div>
